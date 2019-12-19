367aad91c1371845fe57990807ee191030fa2bc3
ichistov <ichistov@eng.vsofts.com>
2019-12-19 16:44:37 +0300

    Обновлено
    Добавлен импорт библиотеки исключений. Добавлен новый аргумент таймаут в метод __init__.
    Новый метод класса на обработку и перехват NoSuchElementException

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,21 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     # Конструктор метод для экземпляра драйвера и адрес сайта
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser: WebDriver = browser
         self.url = url
-        
+        self.browser.implicitly_wait(timeout)
+
     # Открытие нужного адреса сайта
     def open(self):
         self.browser.get(self.url)
+
+    # Перехват исключений
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
